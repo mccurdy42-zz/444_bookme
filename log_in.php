e1b28abd78c5e7ec499dd89d54234e4303c5c0ba
ajacobs12 <allisonjacobs97@gmail.com>
2018-03-20 22:40:16 -0400

    Add files via upload

--- log_in.php
+++ log_in.php
@@ -1,8 +1,8 @@
 <?php
+session_start();
 
 $username =  $_POST['username'];
 $passwordSubmitted =  $_POST['password'];
-//echo $username;
 
 $servername = "localhost";
 $username = "s2mccurd";
@@ -19,7 +19,7 @@ if ($conn->connect_error) {
 //echo "here";
      $sql = 'SELECT seller_password FROM login WHERE seller_email = ?';
      $un = $_POST['username'];
-     echo $un;
+  //   echo $un;
 
      $stmt = $conn->prepare($sql);
 
@@ -30,23 +30,44 @@ if ($conn->connect_error) {
      while ($stmt->fetch()){
       //  echo 'Password: '.$seller_password.'<br><br>';
         $password = $seller_password;
-        echo $password;
-        if (empty('$password')){
-          echo "dne";
-        }
+      //  echo $password;
+
      }
 
-     $stmt->free_result();
-     $stmt->close();
-     $conn->close();
-echo $password;
-echo $passwordSubmitted;
+ $stmt->free_result();
+ $stmt->close();
+
+ $sqlq = 'SELECT seller_ID FROM login WHERE seller_email = ?';
+ $un1 = $_POST['username'];
+//   echo $un;
+
+ $stmt2 = $conn->prepare($sqlq);
+
+ $stmt2->bind_param('s', $un1);
+ $stmt2->execute();
+ $stmt2->store_result();
+ $stmt2->bind_result($seller_ID);
+ while ($stmt2->fetch()){
+  //  echo 'Password: '.$seller_password.'<br><br>';
+    $_SESSION['seller_ID'] = $seller_ID;
+  //  echo $password;
+
+ }
+
+
+ $stmt2->free_result();
+ $stmt2->close();
+ $conn->close();
+
+//testing password entered to the one grabbed for the username entered
 $test = strcmp($password, $passwordSubmitted);
+
+
 if ($test == 0){
+  //need to grab the seller_ID and open the correct seller dashboard accordingly
 header('Location: DB2.php');
 }else{
-  header('Location: log_in2.html');
-
+ header('Location: log_in2.html');
 }
 
 
