2698aadfffca48253cc1779191dcdff2a90d4d0a
ahopkins19 <35613551+ahopkins19@users.noreply.github.com>
2018-03-23 21:27:34 -0400

    Update my_connect.php

--- my_connect.php
+++ my_connect.php
@@ -4,9 +4,9 @@ function get_mysqli_conn()
 {
   
 $dbhost = 'localhost';
-$dbuser = 'ahopkins';
+$dbuser = 's2mccurd';
 $dbpassword = 'Winter@*%2018'; 
-$dbname = 'ahopkins';
+$dbname = 's2mccurd';
 $mysqli = new mysqli($dbhost, $dbuser, $dbpassword, $dbname);
 if ($mysqli->connect_errno) {
     echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error . '\n';
